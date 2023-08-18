5218d6aecbb4b5497f71afdc4052a870ccbe5af9
take3315 <take3315.dfgc@gmail.com>
2023-08-18 19:52:20 +0800

    added CJPY, ran format and lint

--- registercommand.js
+++ registercommand.js
@@ -1,20 +1,25 @@
-const { SlashCommandBuilder } = require('@discordjs/builders'); 
-const { REST } = require('@discordjs/rest'); 
-const { Routes } = require('discord-api-types/v9'); 
-require('dotenv').config();
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-const DISCORD_CLIENT_ID  = process.env.DISCORD_CLIENT_ID;
+const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
 const DISCORD_GUILD_ID = process.env.DISCORD_GUILD_ID;
 
-
 const commands = [
-    new SlashCommandBuilder().setName('report').setDescription('月次活動係数の報告'), 
-    new SlashCommandBuilder().setName('tokenchange').setDescription('受取トークンの変更'), 
-]
-    .map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("report")
+    .setDescription("月次活動係数の報告"),
+  new SlashCommandBuilder()
+    .setName("tokenchange")
+    .setDescription("受取トークンの変更"),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '9' }).setToken(DISCORD_TOKEN);
+const rest = new REST({ version: "9" }).setToken(DISCORD_TOKEN);
 
-rest.put(Routes.applicationGuildCommands(DISCORD_CLIENT_ID, DISCORD_GUILD_ID), { body: commands }) 
-    .then(() => console.log('Successfully registered application commands.'))
-    .catch(console.error)
+rest
+  .put(Routes.applicationGuildCommands(DISCORD_CLIENT_ID, DISCORD_GUILD_ID), {
+    body: commands,
+  })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
